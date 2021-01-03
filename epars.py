79d1ac3fa1d07996217eba825f39e8a0baea5f22
John <50677710+jhnwr@users.noreply.github.com>
2021-01-03 22:27:05 +0000

    Update epars.py
    rewrite to include data extracting including pagination

--- epars.py
+++ epars.py
@@ -1,51 +1,52 @@
-# from helium import *
+# Imports required
 import requests
 from bs4 import BeautifulSoup
+import pandas as pd
+import time
 
+# main page url
 url= 'https://www.ema.europa.eu/en/medicines/ema_group_types/ema_medicine'
-page = requests.get(url)
-
-# browser = start_chrome(url)
-
-# if Text('I accept cookies').exists():
-#     click("I accept cookies")
-
-# while Text('LOAD MORE').exists():
-#     click('LOAD MORE')
-#     time.sleep(4)
-#     if not Text('LOAD MORE').exists(): break
-
-
-epar_name_list = BeautifulSoup.find(class_ ='ema-listings view-content-solr ecl-u-pt-m')
-epar_name_list_items = epar_name_list.find_all('a')
-
-# Create a BeautifulSoup object
-soup = BeautifulSoup(page.text, 'html.parser')
 
 def get_data(url):
+    #takes each page and returns the text from the response
     r = requests.get(url)
-    data = dict(r.json())
-    return data
+    return r.text
 
-def parse(data):    
-    # Pull all titles and descriptions
-    for epar_name in epar_name_list_items:
-        title = epar_name.find(class_='ecl-list-item__title ecl-heading').text.strip()
-        link = "https://www.ema.europa.eu" + epar_name.get('href')
-        description = epar_name.find(class_='ema-u-color-grey-2 ecl-u-mb-s').text.strip()
+def get_pages(data):
+    #using the r.text from the get_data function, creates a soup and works out how many pages are needed to complete the scrape
+    soup = BeautifulSoup(data, 'html.parser')
+    total = soup.find('h3', {'class': 'ecl-heading'}).text.replace(' ','').replace('results','').strip()
+    return round(int(total) / 25, 0)
+
+def parse(data):
+    #parse and return data in a list of dicts. 
+    eparlist = []
+    soup = BeautifulSoup(data, 'html.parser')
+    epars = soup.find_all('li', {'class': 'ecl-list-item'})
+    for epar in epars:
         myepar = {
-                'title': title,
-                'link': link,
-                'description': description
+            'title': epar.find('h3', {'class': 'ecl-list-item__title'}).text.strip().replace('\n','').replace(' ',''),
+            'link': 'https://www.ema.europa.eu' + str(epar.find('a', {'class': 'ecl-link ecl-list-item__link'})['href']),
+            'desc': epar.find('div', {'class': 'ema-u-color-grey-2 ecl-u-mb-s'}).text.replace('\n','').replace(' ','').strip()
         }
-    return myepar
+        eparlist.append(myepar)
+    return eparlist 
 
 def output(results):
-    myepar.to_csv('epars.csv', index=False)    
+    # concat the list of list of dicts into a single dataframe for exporting nb - not the tidiest solution.
+    resultsdf = pd.concat([pd.DataFrame(g) for g in results])
+    resultsdf.to_csv('epars.csv', index=False)    
     return
 
+#run our functions and output the data
 results = []
 data = get_data(url)
-results(parse(data))
+pages = int(get_pages(data))
+for x in range(1, 10):
+    get_data(f'https://www.ema.europa.eu/en/medicines/ema_group_types/ema_medicine?page={x}')
+    results.append(parse(data))
+    print('Getting page: ', x)
+    time.sleep(1)
+
 output(results)
 
